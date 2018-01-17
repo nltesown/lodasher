635b626b347416275ae4cc84f2d5574d81ba8cbc
nltesown <nlte@magic.fr>
2018-01-17 11:08:04 +0100

    Count output items + interpret invalid JSON input as array

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,42 @@ function main () {
   $("button").on("click", function () {
     var lodash =  editor.lodash.getValue();
     var input =  editor.input.getValue();
+    var output;
+
+
+
+    // If the input can't be JSON-parsed, interpret it as an array where each line is an item
+    try {
+      JSON.parse(input);
+    } catch (e) {
+      input = JSON.stringify(
+        _(input.split("\n"))
+        .filter(d => d !== "") // Remove white lines (TODO: improve)
+        .map(d => isNaN(d) ? d : (d * 1))
+        .value()
+      );
+    }
 
     var f = lodash.replace(/data/g, "(" + input + ")"); // Replace `data` by the input globally (the naive way)
     // var f = lodash.replace(/^_\(data\)\s*\./, "_(" + input + ").");
 
+    output = ev();
+
+    // Automatically call value() if necessary
+    if (output instanceof _) {
+      output = output.value();
+    }
+
+    if (typeof output === "object" && typeof output.length !== "undefined") {
+      $(".outlength").html(" (" + output.length + " items)");
+    } else {
+      $(".outlength").html("");
+    }
+
+
     editor.output.setValue(
       js_beautify(
-        JSON.stringify(ev()),
+        JSON.stringify(output),
         {
           "indent_size": 2,
           "brace_style": "expand"
